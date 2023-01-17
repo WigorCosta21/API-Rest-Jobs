0d30b4ea9c9ce5b1d8b8babddb821fdae57b7a73
Wigor <wigor21@hotmail.com>
2023-01-16 23:01:28 -0300

    configuring database

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+// src/app.ts
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import { Candidate } from './models/cadidate'
+const app = express()
+
+const router = express.Router()
+
+router.get('/', (req, res) => res.json({ hello: 'Hello, world!' }))
+
+router.get('/candidates', async (req, res) => {
+  const candidates = await Candidate.findAll()
+  return res.json(candidates)
+})
+
+app.use(router)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('Started!')
+})
